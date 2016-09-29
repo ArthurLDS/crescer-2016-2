b4e328ec374dcd39cf55fe833851dd27e2749224
ArthurLDS <arthurlds73@gmail.com>
2016-09-29 08:38:53 -0300

    feat(Dwarf): novo metodo construtor

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -1,10 +1,13 @@
 public class Dwarf {
+    private String nome;
     private int vida;
     private DataTerceiraEra dataNascimento;
     
     // java type initializer
     // vai ser replicado para cada construtor
-    {
+    public Dwarf(String nome, DataTerceiraEra data){
+        this.nome = nome;
+        this.dataNascimento = data;
         vida = 110;
     }
 
