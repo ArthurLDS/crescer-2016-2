b02d33e72170ee81b115f7ac6e1769f37f706a4b
ArthurLDS <arthurlds73@gmail.com>
2016-09-29 09:39:35 -0300

    test(Dwarf): implementar cenarios de testes para getNumeroSorte

--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -51,4 +51,38 @@ public class DwarfTest
         gimli.perderVida();
         assertEquals(-90, gimli.getVida());
     }
+    @Test
+    public void numSorteNBissextoSeixas(){
+        Dwarf teste = new Dwarf("Seixas", new DataTerceiraEra(1,1,2014));
+        teste.getNumeroSorte();
+        assertEquals(33, teste.getNumeroSorte(),33);
+        //Não sei porque, mas funcionou :)
+    }
+    @Test
+    public void numSorteNBissextoMeireles(){
+        Dwarf teste = new Dwarf("Meireles", new DataTerceiraEra(1,1,2011));
+        teste.getNumeroSorte();
+        assertEquals(33, teste.getNumeroSorte(), 33);
+    }
+    @Test
+    public void numSorteBissextoHpNoventa(){
+        Dwarf teste = new Dwarf("nada", new DataTerceiraEra(1,1,2016));
+        teste.perderVida();
+        teste.perderVida();
+        teste.getNumeroSorte();
+        assertEquals(-3333.0, teste.getNumeroSorte(), -3333.0);
+    }
+    @Test
+    public void numSorteBissextoHpSetenta(){
+        Dwarf teste = new Dwarf("nada", new DataTerceiraEra(1,1,2016));
+        teste.perderVida();
+        teste.perderVida();
+        teste.perderVida();
+        teste.perderVida();
+        teste.perderVida();
+        teste.getNumeroSorte();
+        assertEquals(-3333.0, teste.getNumeroSorte(), -3333.0);
+    }
+    
+    
 }
