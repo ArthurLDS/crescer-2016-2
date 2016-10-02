31f5bb4d8c70529ef025e5b1ed78d0527bfa57e9
ArthurLDS <arthurlds73@gmail.com>
2016-10-02 20:22:19 -0300

    test(HobbitContador): implementar novos ambientes de testes para o metodo calcularDiferenca.

--- src/modulo-01-java-OO/modulo-01/HobbitContadorTest.java
+++ src/modulo-01-java-OO/modulo-01/HobbitContadorTest.java
@@ -18,9 +18,40 @@ public class HobbitContadorTest
         assertEquals(840 ,soma);
     }
     @Test
+    public void calculaProdDasSomaArrayComTudoEm1(){
+        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1,1)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1, 1)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1, 1)));
+        
+        HobbitContador contador = new HobbitContador();
+        int soma = contador.calcularDiferenca(arrayDePares);
+        assertEquals(0 ,soma);
+    }
+    @Test
+    public void calculaProdDasSomaArrayCom6linhas(){
+        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        arrayDePares.add(new ArrayList<>(Arrays.asList(15,18)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(12, 60)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1,1)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1, 1)));
+        arrayDePares.add(new ArrayList<>(Arrays.asList(1, 1)));
+        
+        HobbitContador contador = new HobbitContador();
+        int soma = contador.calcularDiferenca(arrayDePares);
+        assertEquals(840 ,soma);
+    }
+    @Test
     public void calculaMMC4e5(){
         HobbitContador hobbit = new HobbitContador();
         int mmc = hobbit.calculaMmc(4,5);
         assertEquals(20, mmc);
+    }
+    @Test
+    public void calculaMMC1e1(){
+        HobbitContador hobbit = new HobbitContador();
+        int mmc = hobbit.calculaMmc(1,1);
+        assertEquals(1, mmc);
     }
 }
