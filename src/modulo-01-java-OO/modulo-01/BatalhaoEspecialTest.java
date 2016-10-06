76837ff7359ecb9f3d993f9544b95a6699560702
Arthur Lima de Souza <arthurlds73@gmail.com>
2016-10-06 14:08:20 -0300

    test(BatalhaoEspecial): implementação de novos cenários de teste.

--- src/modulo-01-java-OO/modulo-01/BatalhaoEspecialTest.java
+++ src/modulo-01-java-OO/modulo-01/BatalhaoEspecialTest.java
@@ -7,27 +7,29 @@ public class BatalhaoEspecialTest{
     @Test
     public void alistaUmElfoEPesquisa(){
         BatalhaoEspecial batalhao = new BatalhaoEspecial();
-        Elfo elfo = new Elfo("Elfão",80);
+        Elfo elfo = new ElfosVerdes("Elfão",80);
         batalhao.alistar(elfo);
         assertEquals(elfo, batalhao.buscar(elfo.getNome()));
     }
+
     @Test
     public void alistaDoisElfosEPesquisaOPrimeiro(){
         BatalhaoEspecial batalhao = new BatalhaoEspecial();
-        Elfo elfo = new Elfo("Elfão",80);
-        Elfo elfo2 = new Elfo("Elfão2",80);
+        Elfo elfo = new ElfosVerdes("Elfão",80);
+        Elfo elfo2 = new ElfosVerdes("Elfão2",80);
         batalhao.alistar(elfo);
         batalhao.alistar(elfo2);
         assertEquals(elfo, batalhao.buscar(elfo.getNome()));
     }
+
     @Test
     public void alistaVariosElfosEPesquisa(){
         BatalhaoEspecial batalhao = new BatalhaoEspecial();
-        Elfo elfo1 = new Elfo("Elfão7",80);
-        Elfo elfo2 = new Elfo("Elfão78",80);
-        Elfo elfo = new Elfo("Elfão da Massa",80);
-        Elfo elfo3 = new Elfo("Elfão5",80);
-        Elfo elfo4 = new Elfo("Elfão4",80);
+        Elfo elfo1 = new ElfosVerdes("Elfão7",80);
+        Elfo elfo2 = new ElfosVerdes("Elfão78",80);
+        Elfo elfo = new ElfosVerdes("Elfão da Massa",80);
+        Elfo elfo3 = new ElfosVerdes("Elfão5",80);
+        Elfo elfo4 = new ElfosVerdes("Elfão4",80);
         batalhao.alistar(elfo);
         batalhao.alistar(elfo1);
         batalhao.alistar(elfo2);
@@ -35,33 +37,51 @@ public class BatalhaoEspecialTest{
         batalhao.alistar(elfo4);
         assertEquals(elfo, batalhao.buscar("Elfão da Massa"));
     }
+
     @Test
     public void alistaZeroElfosEPesquisaVoltaNull(){
         BatalhaoEspecial batalhao = new BatalhaoEspecial();
         assertNull(batalhao.buscar("Paulão"));
     }
-    
-    //@Test
+
+    @Test
     public void alista1ElfoEPesquisaVivo(){
         BatalhaoEspecial batalhao = new BatalhaoEspecial();
-        Elfo elfo = new Elfo("Elfão",80);
-        batalhao.alistar(elfo);
+        batalhao.alistar(new ElfosVerdes("Elfo da Mascada",80));
         assertEquals(1, batalhao.buscarPorStatus(Status.VIVO).size());
     }
+
     @Test
     public void alista1ElfoEPesquisaMorto(){
-        
+        BatalhaoEspecial batalhao = new BatalhaoEspecial();
+        ElfosNoturnos elfo =  new ElfosNoturnos("Elfo da Mascada");
+        for(int i=0; i<=90; i++)
+            elfo.perderVida();
+        batalhao.alistar(elfo); 
+        assertEquals(1, batalhao.buscarPorStatus(Status.MORTO).size());
     }
+
     @Test
-    public void alista5ElfosEPesquisaMortos(){
-        
+    public void alista2ElfosEPesquisaMortos(){
+        BatalhaoEspecial batalhao = new BatalhaoEspecial();
+        ElfosNoturnos elfo =  new ElfosNoturnos("Elfo da Mascada");
+        for(int i=0; i<=90; i++)
+            elfo.perderVida();
+        ElfosNoturnos elfo1 =  new ElfosNoturnos("Elf da Mascada");
+        for(int i=0; i<=90; i++)
+            elfo1.perderVida();
+        batalhao.alistar(elfo);
+        batalhao.alistar(elfo1);
+        assertEquals(2, batalhao.buscarPorStatus(Status.MORTO).size());
     }
+
     @Test
     public void alista5ElfosEPesquisaVivos(){
-        
+
     }
+
     @Test
     public void alistaZeroElfosEPesquisaVivos(){
-        
+
     }
 }
