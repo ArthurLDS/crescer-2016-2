611d1bbb16e99082f7dd54d514eb8a58c6f1e320
ArthurLDS <arthurlds73@gmail.com>
2016-10-06 09:53:02 -0300

    test(ElfosVerdes): otimização de testes.

--- src/modulo-01-java-OO/modulo-01/ElfosVerdesTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfosVerdesTest.java
@@ -7,41 +7,41 @@ public class ElfosVerdesTest
 {
     @Test
     public void ElfoVerdeAtiraFlechaERecebe2deXP(){
-        ElfosVerdes teste = new ElfosVerdes("");
-        teste.atirarFlechaDeVidro(new Dwarf());
+        ElfosVerdes teste = new ElfosVerdes("", 40);
+        teste.atirarFlecha(new Dwarf());
         assertEquals(2, teste.getExperiencia());
     }
     @Test
     public void ElfoVerdeNaoAtiraFlechaENRecebeXP(){
-        ElfosVerdes teste = new ElfosVerdes("");
+        ElfosVerdes teste = new ElfosVerdes("", 40);
         assertEquals(0, teste.getExperiencia());
     }
     @Test
     public void ElfoVerdeAtiraFlechaEPerdeUmaFlecha(){
-        ElfosVerdes teste = new ElfosVerdes("");
-        teste.atirarFlechaDeVidro(new Dwarf());
-        int quantFlechas = teste.getFlechaDeVidro().getQuantidade();
+        ElfosVerdes teste = new ElfosVerdes("", 42);
+        teste.atirarFlecha(new Dwarf());
+        int quantFlechas = teste.getFlecha().getQuantidade();
         assertEquals(41, quantFlechas);
     }
     @Test
     public void ElfoVerdeNAtiraFlechaENPerdeFlecha(){
-        ElfosVerdes teste = new ElfosVerdes("");
-        int quantFlechas = teste.getFlechaDeVidro().getQuantidade();
+        ElfosVerdes teste = new ElfosVerdes("", 42);
+        int quantFlechas = teste.getFlecha().getQuantidade();
         assertEquals(42, quantFlechas);
     }
     @Test
     public void ElfoVerdeAtira42FlechasEPerdeTodasFlechas(){
-        ElfosVerdes teste = new ElfosVerdes("");
+        ElfosVerdes teste = new ElfosVerdes("", 40);
         for(int i=0; i<42; i++)
-            teste.atirarFlechaDeVidro(new Dwarf());
-        int quantFlechas = teste.getFlechaDeVidro().getQuantidade();
+            teste.atirarFlecha(new Dwarf());
+        int quantFlechas = teste.getFlecha().getQuantidade();
         assertEquals(0, quantFlechas);
     }
     @Test
     public void ElfoVerdeAtira42FlechasEXpFica84(){
-        ElfosVerdes teste = new ElfosVerdes("");
+        ElfosVerdes teste = new ElfosVerdes("", 42);
         for(int i=0; i<42; i++)
-            teste.atirarFlechaDeVidro(new Dwarf());
+            teste.atirarFlecha(new Dwarf());
         assertEquals(84, teste.getExperiencia());
     }
 }
