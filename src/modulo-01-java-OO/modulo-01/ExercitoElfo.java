f42e2e8ed828ce4275c155eb04d721b493f942a8
ArthurLDS <arthurlds73@gmail.com>
2016-10-08 14:05:49 -0300

    refactor(ExercitoElfo): otimização método getOrdemDeAtaque.

--- src/modulo-01-java-OO/modulo-01/ExercitoElfo.java
+++ src/modulo-01-java-OO/modulo-01/ExercitoElfo.java
@@ -35,27 +35,24 @@ public class ExercitoElfo{
 
     public ArrayList<Elfo> buscarPorStatus(Status status){
         ArrayList<Elfo> escolhidosPorStatus = new ArrayList<>();
-        for(int i=0; i<exercito.size(); i++){
-            if(exercito.get(i).getStatus().equals(status)){
+        for(int i=0; i<exercito.size(); i++)
+            if(exercito.get(i).getStatus().equals(status))
                 escolhidosPorStatus.add(exercito.get(i));
-            }
-        }
         return escolhidosPorStatus;
     }
 
     public List<Elfo> getOrdemDeAtaque(List<Elfo> atacantes, List<Dwarf> alvos){
         Elfo aux = null;
-        for(int i=atacantes.size()-1; i>=0; i--){
-            if(atacantes.get(i).getStatus() == Status.MORTO){
+        for(int i=atacantes.size()-1; i>=0; i--)
+            if(atacantes.get(i).getStatus() == Status.MORTO)
                 atacantes.remove(i);
                 
-            }
-        }
         for(int i=atacantes.size()-1; i>=1; i--){
-            for(int j=0; j<i; j++){
-                if(atacantes.get(j) instanceof ElfosNoturnos && atacantes.get(j+1) instanceof ElfosVerdes){
+            for(int j=0; j<i; j++){     //Esse elfoPost aí é de "Elfo Posterior"!
+                Elfo elfoAtual = atacantes.get(j), elfoPost = atacantes.get(j+1);
+                if(elfoAtual instanceof ElfosNoturnos && elfoPost instanceof ElfosVerdes){
                     aux = atacantes.get(j);
-                    atacantes.set(j, atacantes.get(j+1));
+                    atacantes.set(j, elfoPost);
                     atacantes.set(j+1, aux);
                 }
             }
