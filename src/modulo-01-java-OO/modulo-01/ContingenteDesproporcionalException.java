2939f6b6141496124ab06d4f8fbb9861f7e87140
ArthurLDS <arthurlds73@gmail.com>
2016-10-09 13:58:33 -0300

    feat(ContingenteDesproporcionalException): nova classe de exception.

---
+++ src/modulo-01-java-OO/modulo-01/ContingenteDesproporcionalException.java
@@ -0,0 +1,9 @@
+public class ContingenteDesproporcionalException extends Exception
+{
+    public ContingenteDesproporcionalException() {
+        super("Contingentes desproporcionais, não foi possível terminar a operação. Tente novamente.");
+    }
+
+    
+
+}
