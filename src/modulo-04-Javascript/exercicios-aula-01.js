6bdf3c1ebbbfff0546107987d2110d08078ba463
Arthur Lima de Souza <arthurlds73@gmail.com>
2016-10-24 17:24:57 -0200

    feat(ExerciciosJS): novos exercicios.

---
+++ src/modulo-04-Javascript/exercicios-aula-01.js
@@ -0,0 +1,36 @@
+// Exercicio 01
+function gerarPiramide(niveis) {
+  for(var i=1; i<=niveis; i++)
+    for(var j=1; j<=i; j++){
+      console.log('R$ ');
+    }
+}
+
+// Exercicio 02
+function diglettDig() {
+  for(var i=1; i<=100; i++){
+    if(i%3 === 0 && i%5 === 0)
+      console.log('Diglett dig, trio trio trio');
+    else if(i%3 === 0)
+      console.log('Diglett dig');
+    else if (i%5 === 0)
+      console.log('trio trio trio');
+    else
+      console.log(i);
+  }
+}
+
+// Exercicio 03
+function find(array, funcao){
+    var result = '';
+    for(var i=0; i< array.length; i++){
+        if(funcao(array[i])){
+          result += array[i] + ' ';
+        }
+    }
+    return result;
+}
+
+function subtrair(){
+
+}
