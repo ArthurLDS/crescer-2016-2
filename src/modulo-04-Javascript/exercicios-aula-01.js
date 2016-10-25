d0d24011829724cb863156b0f961eff428855ccc
ArthurLDS <arthurlds73@gmail.com>
2016-10-25 08:59:16 -0200

    feat(Exercicios): implementação exercicios 5 e 7.

--- src/modulo-04-Javascript/exercicios-aula-01.js
+++ src/modulo-04-Javascript/exercicios-aula-01.js
@@ -1,10 +1,7 @@
 // Exercicio 01
 function gerarPiramide(niveis) {
   for(var i=1; i<=niveis; i++)
-    for(var j=1; j<=i; j++){
-      console.log('R$ ');
-    }
-    console.log(' ');
+       console.log(Array(i+1).join('R$'));
 }
 
 // Exercicio 02
@@ -32,11 +29,53 @@ function find(array, funcao){
     return result;
 }
 
-
-
+// Exercicio 04
 function subtrair(n1){
   function subtrairn2(n2){
     return subtracao = n1-n2;
   }
   return subtrairn2;
 }
+
+/* Exercicio 05 Incompleto
+function iguais(n1, n2){
+  if(typeof n1 === Object && typeof n2 === Object){
+
+  }
+  else if(n1 === n2)  
+    return true;
+  else
+    return false;
+}*/
+
+/* 06
+function ctrlC(valor){
+ 
+  return valor;
+}*/
+
+
+// Exercicio 07 C/ BÔNUS
+function mesclar(obj1, obj2){
+  for(i in obj1){
+    for(j in obj2){
+      if(typeof obj1[i] == Object && typeof obj2[j])
+          Object.assign(obj1[i], obj2[j]);
+    }
+  }
+  Object.assign(obj1, obj2);
+
+}
+
+/* Teste
+var objeto1 = {
+  abacaxi: 0,
+  banana: { peso: 52, preco: 100 },
+  cereja: 97
+};
+var objeto2 = {
+  banana: { preco: 200 },
+  damasco: 100
+};
+mesclar(objeto1, objeto2);
+console.log(objeto1); */
