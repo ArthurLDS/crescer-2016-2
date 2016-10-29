cc0f2eb6b82018c14d8bd6d05263fe549602c3f5
ArthurLDS <arthurlds73@gmail.com>
2016-10-28 23:22:17 -0200

    feat(Pokemom): implementar últimos detalhes.

---
+++ src/modulo-05-C#-prt1/MarioKart/NÃO CORRIGIR/ConsoleApplication3/Corredor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public class Corredor
+    {
+        public Corredor(string nome, Habilidade habilidade)
+        {
+            this.Nome = nome;
+            this.NivelHabilidade = habilidade;
+        }
+        protected string Nome { get; }  
+        protected Habilidade NivelHabilidade { get; }
+    }
+}
