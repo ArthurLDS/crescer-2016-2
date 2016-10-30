f5777cc8f9611f3f738be6fd81d440a08edacc5c
ArthurLDS <arthurlds73@gmail.com>
2016-10-30 17:31:27 -0200

    feat(ExercicioLambda): implementar exercicios Easy.

--- src/modulo-05-C#-prt1/MarioKart/ConsoleApplication1/ConsoleApplication1/Karts/SkyFusion.cs
+++ src/modulo-05-C#-prt1/MarioKart/ConsoleApplication1/ConsoleApplication1/Karts/SkyFusion.cs
@@ -14,6 +14,7 @@ namespace MarioKart
 
         }
         public int Bonus {
+
             get
             {
                 int totalBonus = Equipamentos.Count*2 + CorredorKart.GetBonusHabilidade()*2;
