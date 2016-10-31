880eaf59b55b4468574fba9af600707b4c5a5f71
Arthur Lima de Souza <arthurlds73@gmail.com>
2016-10-31 17:27:36 -0200

    feat(ExercicioMVC): implementação de exercicio.

---
+++ src/modulo-05-C#-prt1/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,25 @@
+﻿using StreetFighter.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FichaTecnica()
+
+
+            var modelo = new FichaTecnicaModel();
+            modelo.PrimeiraAparicao = "Morro da Perdra.";
+            return View(primeiraAparicao);
+        }
+    }
+}
