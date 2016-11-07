313e050e91f35dfc65ab4d8dbfa85adbdba71ba2
ArthurLDS <arthurlds73@gmail.com>
2016-11-07 09:21:15 -0200

    feat(ServiçoDeUsuario.Aplicativo): implementar chamada de GetUsuarios em repositório.

--- src/modulo-05-C#-prt1/StreetFighter.Web/StreetFighter.aplicativo/ServicoDeUsuario.cs
+++ src/modulo-05-C#-prt1/StreetFighter.Web/StreetFighter.aplicativo/ServicoDeUsuario.cs
@@ -1,5 +1,6 @@
 ﻿using StreetFighter.dominio;
 using StreetFighter.Infra;
+using StreetFighter.repositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,10 @@ namespace StreetFighter.aplicativo
 {
     public class ServicoDeUsuario
     {
-        private static Usuario[] _usuarios =
-        {
-            new Usuario()
-            {
-                Nome = "joao",
-                Senha = "f6a47bad775bde570f264d85871d9ae8",
-                Permissoes = new string[] { "CHINELO" }
-            }
-        };
+        public ServicoUsuarioRepositorio UsuarioRepositorio = new ServicoUsuarioRepositorio();
+
+        private static Usuario[] _usuarios = new ServicoUsuarioRepositorio().GetUsuarios();
+        
 
         public static Usuario BuscarUsuarioAutenticado(string nome, string senha)
         {
