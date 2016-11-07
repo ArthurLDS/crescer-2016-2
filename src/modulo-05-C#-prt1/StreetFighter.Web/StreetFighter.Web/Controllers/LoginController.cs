c98106af2ca565577fabebb1c25107107edd6996
ArthurLDS <arthurlds73@gmail.com>
2016-11-07 07:56:35 -0200

    feat(StreetFighter): implementar serviço de criptografia.

---
+++ src/modulo-05-C#-prt1/StreetFighter.Web/StreetFighter.Web/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using StreetFighter.aplicativo;
+using StreetFighter.dominio;
+using StreetFighter.Web.Filters;
+using StreetFighter.Web.Models;
+using StreetFighter.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("StreetFighter");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult FazerLogin(string usuario, string senha)
+        {
+            Usuario usuarioAutenticado = ServicoDeUsuario.BuscarUsuarioAutenticado(
+                    usuario, senha);
+
+            if (usuarioAutenticado != null)
+            {
+                ServicoDeAutenticacao.Autenticar(new UsuarioLogadoModel(
+                    usuarioAutenticado.Nome, usuarioAutenticado.Permissoes));
+                return RedirectToAction("Secreta");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [CwiAutorizador]
+        public ActionResult Secreta()
+        {
+            return View();
+        }
+
+        [CwiAutorizador(Roles = "DIRETOR")]
+        public ActionResult SuperSecreta()
+        {
+            return View();
+        }
+    }
+}
