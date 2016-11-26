d234f4dd221f4b17a5acf9d1d9ec1ff765cffac6
ArthurLDS <arthurlds73@gmail.com>
2016-11-26 20:11:08 -0200

    feat(ExerciciosDeArquivos): implementar novas funcionabilidades ao médodo Run.

--- src/modulo-08-Java/exerciciosDeArquivos/src/main/java/br/com/cwi/crescer/Run.java
+++ src/modulo-08-Java/exerciciosDeArquivos/src/main/java/br/com/cwi/crescer/Run.java
@@ -1,12 +1,24 @@
 package br.com.cwi.crescer;
 
 import br.com.cwi.crescer.MeuFileUtils;
+import br.com.cwi.crescer.MeuReaderUtils;
+import br.com.cwi.crescer.MeuWriterUtils;
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.Scanner;
 
 public class Run {
 
     public static void main(String[] args) {
         //C:\\Users\\Arthur\\Documents\\Projeto Crescer\\GitHub\\crescer-2016-2\\src\\modulo-08-Java\\exerciciosDeArquivos\\
+        
+        /*List<String> lista = new ArrayList<String>();
+        lista.add("Oi");
+        lista.add("thau");
+        
+        MeuWriterUtils.escreverConteudo("naoExiste.txt", lista);
+        MeuReaderUtils.exibirConteudo("teste.txt");*/
          
         Scanner ler = new Scanner(System.in);
         do {
@@ -19,7 +31,6 @@ public class Run {
             }
             String comando = MeuFileUtils.getComando(instrucao);
             String caminho = MeuFileUtils.getCaminho(instrucao);
-
             
             switch (comando) {
                 case "mk":
@@ -31,10 +42,30 @@ public class Run {
                 case "ls":
                     System.out.println("Caminho: " + MeuFileUtils.mostrarCaminhoAbsoluto(caminho));
                     break;
+                //Leitura de Arquivo (Exercicio-02)    
+                case "read":
+                    MeuReaderUtils.exibirConteudo(caminho);
+                    break;
+                //Escrita em Arquivo (Exercicio-03)
+                case "write":
+                    List<String> conteudos = new ArrayList<>();
+                    String resposta = "N";
+                    do{
+                        System.out.print("Insira um texto: ");
+                        conteudos.add(ler.nextLine());
+                        
+                        System.out.print("Deseja inserir mais textos? [S]im [N]ão: ");
+                        resposta = ler.nextLine();
+                        
+                    }while(resposta.charAt(0) != 'N');
+                    
+                    MeuWriterUtils.escreverConteudo(caminho, conteudos);
+                        
                 default:
                     System.out.println("Comando inválido!");
             }
         } while (true);
-
+        
+        
     }  
 }
