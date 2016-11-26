611c36b521f4568b463d75d52715e61defa1bdb9
ArthurLDS <arthurlds73@gmail.com>
2016-11-26 17:32:09 -0200

    feat(MeuFileUtils): implementar leitura de coamdos na classe Run.

--- src/modulo-08-Java/exerciciosDeArquivos/src/main/java/br/com/cwi/crescer/Run.java
+++ src/modulo-08-Java/exerciciosDeArquivos/src/main/java/br/com/cwi/crescer/Run.java
@@ -1,17 +1,51 @@
 package br.com.cwi.crescer;
 
 import br.com.cwi.crescer.MeuFileUtils;
+import java.util.Scanner;
 
 public class Run {
+
     public static void main(String[] args) {
-        String caminho = "teste.txt";
-        
-        boolean ok = MeuFileUtils.criarArquivo(caminho);
-        if(ok) System.out.println("Arquivo criado!");
-        
-        System.out.println("Caminho deste arquivo: " + MeuFileUtils.mostrarCaminhoAbsoluto(caminho));
-        /*boolean deleteOk = MeuFileUtils.excluirArquivo(caminho);
-        if(deleteOk) System.out.println("Arquivo excluído!!");
-        else System.out.println("Opá isso é um diretorio!");*/
+        //C:\\Users\\Arthur\\Documents\\Projeto Crescer\\GitHub\\crescer-2016-2\\src\\modulo-08-Java\\exerciciosDeArquivos\\
+         
+        Scanner ler = new Scanner(System.in);
+        do {
+            System.out.print("Digite um comando: ");
+            String instrucao = ler.nextLine();
+            
+            if(!validaComando(instrucao)){ 
+                System.out.println("Comando inválido!");
+                continue;
+            }
+            
+            String comando = getComando(instrucao);
+            String caminho = getCaminho(instrucao);
+
+            switch (comando) {
+                case "mk":
+                    MeuFileUtils.criarArquivo(caminho);
+                    break;
+                case "rm":
+                    MeuFileUtils.excluirArquivo(caminho);
+                    break;
+                case "ls":
+                    System.out.println("Caminho: " + MeuFileUtils.mostrarCaminhoAbsoluto(caminho));
+                    break;
+                default:
+                    System.out.println("Comando inválido!");
+            }
+        } while (true);
+
+    }
+
+    public static String getComando(String instrucao) {
+        return instrucao.split(" ")[0];
+    }
+
+    public static String getCaminho(String instrucao) {
+        return instrucao.split(" ")[1];
+    }
+    public static boolean validaComando(String instrucao){
+        return instrucao.contains(" ");
     }
 }
