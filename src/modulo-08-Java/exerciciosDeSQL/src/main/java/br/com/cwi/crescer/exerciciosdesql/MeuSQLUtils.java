b95012e972dec55eeb72260d0836c0d530efb869
ArthurLDS <arthurlds73@gmail.com>
2016-11-27 13:51:42 -0200

    feat(MeuSQLUtils): implementar método executaSQL.

--- src/modulo-08-Java/exerciciosDeSQL/src/main/java/br/com/cwi/crescer/exerciciosdesql/MeuSQLUtils.java
+++ src/modulo-08-Java/exerciciosDeSQL/src/main/java/br/com/cwi/crescer/exerciciosdesql/MeuSQLUtils.java
@@ -1,16 +1,45 @@
 package br.com.cwi.crescer.exerciciosdesql;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MeuSQLUtils {
     
     public void executaSQLDeArquivo(String query){
     
     }
+    
     public void executarSQL(String query){
         
+        try{
+            final Connection conexao = ConexaoUtils.getConexao();
+            final Statement statement = conexao.createStatement();
+            
+            try{
+                final ResultSet resultSet = statement.executeQuery(query);
+                
+                while(resultSet.next()) {
+                    final long idPessoa = resultSet.getLong("ID_PESSOA");
+                    final String nmPessoa = resultSet.getString("NM_PESSOA");
+                    
+                    System.out.format("ID: %s NOME: %s\n", idPessoa, nmPessoa);
+                }
+                
+            }
+            catch(final SQLException e){
+                System.err.format("SQLException: %s", e);
+            }
+        }
+        catch(final SQLException e){
+            System.err.format("SQLException: %s", e);
+        }
     }
     public void importarArquivo(String arquivo){
         
     }
+    
     public void exportarArquivo(String arquivo){
         
     }
