a5020ca7f720be6cfde43c8c5e41191960158c08
ArthurLDS <arthurlds73@gmail.com>
2016-11-27 13:09:12 -0200

    feat(ExerciciosDeSQL): implementar base dos métodos.

---
+++ src/modulo-08-Java/exerciciosDeSQL/src/main/java/br/com/cwi/crescer/exerciciosdesql/MeuSQLUtils.java
@@ -0,0 +1,18 @@
+package br.com.cwi.crescer.exerciciosdesql;
+
+public class MeuSQLUtils {
+    
+    public void executaSQLDeArquivo(String query){
+    
+    }
+    public void executarSQL(String query){
+        
+    }
+    public void importarArquivo(String arquivo){
+        
+    }
+    public void exportarArquivo(String arquivo){
+        
+    }
+    
+}
