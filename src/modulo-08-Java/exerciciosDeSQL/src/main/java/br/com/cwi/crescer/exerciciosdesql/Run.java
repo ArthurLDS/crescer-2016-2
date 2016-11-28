757f38ab787bbf646b4088e535e10c61eb667961
ArthurLDS <arthurlds73@gmail.com>
2016-11-28 10:04:30 -0200

    feat(Run): implementar interface de leitura de dados para usuário.

--- src/modulo-08-Java/exerciciosDeSQL/src/main/java/br/com/cwi/crescer/exerciciosdesql/Run.java
+++ src/modulo-08-Java/exerciciosDeSQL/src/main/java/br/com/cwi/crescer/exerciciosdesql/Run.java
@@ -3,17 +3,33 @@ package br.com.cwi.crescer.exerciciosdesql;
 import java.util.Scanner;
 
 public class Run {
+
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        
-        /*System.out.print("Digite um comando SQL: ");
-        String comandoSQL = ler.nextLine();
-        MeuSQLUtils.executarSQL(comandoSQL);
-        
-        System.out.print("Digite um arquivo SQL: ");
-        String arquivoSQL = ler.nextLine();
-        MeuSQLUtils.executaSQLDeArquivo(arquivoSQL);*/
-        
-        MeuSQLUtils.exportarArquivo("dados.csv");
+        String instrucao = "";
+
+        do {
+            System.out.print("Digite uma instrução: ");
+            instrucao = ler.nextLine();
+
+            switch (instrucao) {
+                case "sql":
+                    System.out.print("Digite um comando SQL: ");
+                    String comandoSQL = ler.nextLine();
+                    MeuSQLUtils.executarSQL(comandoSQL);
+                    break;
+                case "fileSql":
+                    System.out.print("Digite um arquivo .SQL: ");
+                    String arquivoSQL = ler.nextLine();
+                    MeuSQLUtils.executaSQLDeArquivo(arquivoSQL);
+                    break;
+                case "importFile":
+                    System.out.print("Digite um arquivo .CSV");
+                    String arquivo = ler.nextLine();
+                    MeuSQLUtils.importarArquivo(arquivo);
+                    break;
+            }
+        } while (true);
     }
+
 }
