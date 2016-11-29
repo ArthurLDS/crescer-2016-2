1f20ccb4c4f8d31caadc70275d146d5da4067ae3
ArthurLDS <arthurlds73@gmail.com>
2016-11-28 22:43:42 -0200

    feat(ExercicioEntidade): implementar métodos de IDao na classe ClientDao.

--- src/modulo-08-Java/exercicioEntidade/src/main/java/br/com/cwi/crescer/exercicioentidade/dao/ClientDao.java
+++ src/modulo-08-Java/exercicioEntidade/src/main/java/br/com/cwi/crescer/exercicioentidade/dao/ClientDao.java
@@ -6,19 +6,20 @@
 package br.com.cwi.crescer.exercicioentidade.dao;
 
 import br.com.cwi.crescer.exercicioentidade.entity.Client;
+import java.util.List;
 import javax.persistence.EntityManager;
 
 /**
  *
  * @author Arthur
  */
-public class ClientDao {
+public class ClientDao implements IDao<Client, Long>{
     final EntityManager entityManager;
 
     public ClientDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    
+    @Override
     public void insert(Client cliente){
          try {
             entityManager.getTransaction().begin();
@@ -32,4 +33,34 @@ public class ClientDao {
             entityManager.getTransaction().rollback();
         }
     }
+    @Override
+    public void delete(Client cliente){
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.remove(cliente);
+            entityManager.getTransaction().commit();
+        }
+        catch(Exception e){
+            entityManager.getTransaction().rollback();
+        }
+    }
+    @Override
+    public void update(Client cliente){
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.merge(cliente);
+            entityManager.getTransaction().commit();
+        }
+        catch(Exception e){
+            entityManager.getTransaction().rollback();
+        }
+    }
+    
+    @Override
+    public List<Client> list(){
+        List<Client> lista =  entityManager.createQuery("select c from Client c").getResultList();
+        return lista;
+    }
+    
+    
 }
