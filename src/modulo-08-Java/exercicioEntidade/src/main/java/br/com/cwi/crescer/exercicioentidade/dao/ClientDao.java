5cd8d5d0ba3322cbaf19ed59af0f577bf77e0edd
ArthurLDS <arthurlds73@gmail.com>
2016-11-28 23:20:48 -0200

    feat(ExercicioEntidade): implementar método de Exportação de arquivo csv.

--- src/modulo-08-Java/exercicioEntidade/src/main/java/br/com/cwi/crescer/exercicioentidade/dao/ClientDao.java
+++ src/modulo-08-Java/exercicioEntidade/src/main/java/br/com/cwi/crescer/exercicioentidade/dao/ClientDao.java
@@ -1,27 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.cwi.crescer.exercicioentidade.dao;
 
 import br.com.cwi.crescer.exercicioentidade.entity.Client;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.List;
 import javax.persistence.EntityManager;
 
-/**
- *
- * @author Arthur
- */
-public class ClientDao implements IDao<Client, Long>{
+public class ClientDao implements IDao<Client, Long> {
+
     final EntityManager entityManager;
 
     public ClientDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+
     @Override
-    public void insert(Client cliente){
-         try {
+    public void insert(Client cliente) {
+        try {
             entityManager.getTransaction().begin();
             if (cliente.getIdClient() == null) {
                 entityManager.persist(cliente);
@@ -33,34 +29,53 @@ public class ClientDao implements IDao<Client, Long>{
             entityManager.getTransaction().rollback();
         }
     }
+
     @Override
-    public void delete(Client cliente){
-        try{
+    public void delete(Client cliente) {
+        try {
             entityManager.getTransaction().begin();
             entityManager.remove(cliente);
             entityManager.getTransaction().commit();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             entityManager.getTransaction().rollback();
         }
     }
+
     @Override
-    public void update(Client cliente){
-        try{
+    public void update(Client cliente) {
+        try {
             entityManager.getTransaction().begin();
             entityManager.merge(cliente);
             entityManager.getTransaction().commit();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             entityManager.getTransaction().rollback();
         }
     }
-    
+
     @Override
-    public List<Client> list(){
-        List<Client> lista =  entityManager.createQuery("select c from Client c").getResultList();
+    public List<Client> list() {
+        List<Client> lista = entityManager.createQuery("select c from Client c").getResultList();
         return lista;
     }
-    
-    
+
+    public void exportarCSV(Client cliente) {
+        try {
+            final Writer writer = new FileWriter("Client.csv", true);
+            final BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+            bufferedWriter.newLine();
+            bufferedWriter.append(cliente.getDsEmail() + ";");
+            bufferedWriter.append(cliente.getDsPassword() + ";");
+            bufferedWriter.append(cliente.getDsPreferredCoin() + ";");
+            bufferedWriter.append(cliente.getDsState() + ";");
+            bufferedWriter.append(cliente.getDsUserName() + ";");
+            bufferedWriter.append(cliente.getNmClient() + ";");
+            bufferedWriter.append(cliente.getTpPermission() + ";");
+            
+             bufferedWriter.flush();
+            
+        } catch (Exception e) {
+        }
+    }
+
 }
