94b04d26e6ceaa3b5e9a4d89697b41292976d2e3
ArthurLDS <arthurlds73@gmail.com>
2016-12-03 17:04:43 -0200

    feat(ExercicioCadastro): implementar classe UserBean e UserDao.

---
+++ src/modulo-08-Java/ExerciciosLogin/aula5/src/main/java/br/com/cwi/crescer/aula5/bean/UserBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula5.bean;
+
+import br.com.cwi.crescer.aula5.dao.UserDao;
+import br.com.cwi.crescer.aula5.filters.User;
+
+/**
+ *
+ * @author Arthur
+ */
+public class UserBean extends AbstractBean<User, Long, UserDao>{
+    
+    private UserDao userDao;
+    
+    @Override
+    public UserDao getDao() {
+        if (userDao == null) {
+            userDao = new UserDao(this.getEntityManager());
+        }
+        return userDao;
+    }
+    
+}
